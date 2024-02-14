83c270cc0a94667787ed1781d17b58761b6028de
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-02-14 16:47:56 +0200

    Implemented new interface type

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def parse_arguments():
                         help='Path to the folder with input images.')
     parser.add_argument('--output', type=str, default='./output',
                         help='Path to the folder where dataset will be stored.')
+    parser.add_argument('--format', type=str, default='standard',
+                        help='Choose between \'standard\' and \'tficon\' format of COCO dataset.')
+
     return parser.parse_args()
 
 
@@ -17,7 +20,10 @@ if __name__ == '__main__':
     args = parse_arguments()
     print('Settings are: ', args)
     walker = DefaultDirectoryWalker(root=args.root, cache=False)
-    saver = TFIconSaver(output=args.output)
+    if args.format == 'tficon':
+        saver = TFIconSaver(output=args.output)
+    else:
+        saver = DefaultSaver(output=args.output)
     print('Paths are: ', walker.paths)
     labeler = ImageLabeler(saver=saver, directory_walker=walker)
     labeler.start()
