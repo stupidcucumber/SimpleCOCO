4397f89172d81b13e4be713a6a0aa28f1681b742
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-02-14 14:11:08 +0200

    Implemented abstract class for the directory walker

---
+++ src/walker/base.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+
+
+class DirectoryWalker:
+    def __init__(self, root: str, cache: bool=True):
+        self.root = root
+        self.cache = cache
+        self.elements = None
+
+        self.paths = self._extract_paths()
+        if self.cache:
+            self.elements = self._load_images()
+    
+    def _extract_paths(self) -> list[str]:
+        raise NotImplementedError('This function needs to be implemented in the derived class!')
+
+    def _load_image(self, path) -> np.ndarray:
+        raise NotImplementedError('This function needs to be implemented in the derived class!')
+
+    def _load_images(self) -> list[np.ndarray]:
+        result = []
+        for path in self.paths:
+            image = self._load_image(path=path)
+            result.append(image)
+        return result
+
+    def __getitem__(self, index):
+        if self.cache:
+            return self.elements[index]
+        return self._load_image(path=self.paths[index])
