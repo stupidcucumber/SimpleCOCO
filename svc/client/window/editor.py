168d5d678599f05d87acedc9e0b486a9c0ecb54b
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-06-06 10:48:30 +0300

    Implemented controll Toolbar

--- svc/client/window/editor.py
+++ svc/client/window/editor.py
@@ -1,15 +1,20 @@
-from PyQt6.QtCore import QObject
+from typing import List
+from PyQt6.QtCore import QObject, Qt
 from PyQt6.QtWidgets import (
     QMainWindow,
     QScrollArea,
     QLabel,
     QToolBar,
-    QPushButton
+    QPushButton,
+    QWidget,
+    QApplication
 )
 from PyQt6.QtGui import (
+    QAction,
     QImage,
     QPixmap,
-    QIcon
+    QIcon,
+    QCursor
 )
 from ..utils import Connection
 from ...backend.src.structs import (
@@ -26,6 +31,70 @@ from ..request import (
 from ..utils.image import (
     base64tobytes
 )
+
+
+class ControllerToolbar(QToolBar):
+    def __init__(self, parent: QObject | None = None) -> None:
+        super(ControllerToolbar, self).__init__(parent)
+        self._setup_layout()
+        
+    def addWidget(self, widget: QWidget | None) -> QAction | None:
+        raise NotImplementedError('Use addPushButton instead. Adding other widgets is not supported!')
+        
+    def addPushButton(self, push_button: QPushButton) -> QAction | None:
+        if not isinstance(push_button, QPushButton):
+            raise ValueError('Only QPushButton widgets are supported!')
+        return super().addWidget(push_button)
+    
+    def children(self) -> List[QPushButton]:
+        return super().children()
+    
+    def _enable_except(self, button: QPushButton) -> None:
+        for child in self.children():
+            if child != button:
+                child.setEnabled(True)
+            else:
+                child.setDisabled(True)
+                
+    def _instantiate_cursor_button(self) -> QPushButton:
+        button = QPushButton(
+            QIcon(
+                QPixmap('svc/client/graphics/cursor-icon.png')
+            ),
+            ''
+        )
+        button.setDisabled(True)
+        def slot():
+            QApplication.restoreOverrideCursor()
+            self._enable_except(button)
+        button.pressed.connect(slot) 
+        return button
+    
+    def _instantiate_bbox_button(self) -> QPushButton:
+        icon_path = 'svc/client/graphics/bounding-box-icon.png'
+        cursor_pixmap = QPixmap(icon_path)
+        cursor_pixmap = cursor_pixmap.scaledToHeight(32)
+        button = QPushButton(
+            QIcon(
+                QPixmap(icon_path)
+            ),
+            ''
+        )
+        def slot():
+            new_cursor = QCursor(cursor_pixmap)
+            QApplication.setOverrideCursor(new_cursor)
+            self._enable_except(button)
+        button.pressed.connect(slot)
+        return button
+        
+    def _setup_layout(self):
+        self.addPushButton(
+            self._instantiate_cursor_button()
+        )
+        self.addPushButton(
+            self._instantiate_bbox_button()
+        )
+         
 
 
 class EditorWindow(QMainWindow):
@@ -46,30 +115,6 @@ class EditorWindow(QMainWindow):
             ) for annotation in self.annotations
         ]
         self._setup_layout()
-        
-    def _create_add_annotation_button(self) -> QPushButton:
-        button = QPushButton(
-            QIcon(
-                QPixmap('svc/client/graphics/bounding-box-icon.png')
-            ), 
-            ''
-        )
-        return button
-    
-    def _create_cursor_button(self) -> QPushButton:
-        button = QPushButton(
-            QIcon(
-                QPixmap('svc/client/graphics/cursor-icon.png')
-            ),
-            ''
-        )
-        return button
-
-    def _setup_toolbar(self) -> QToolBar:
-        toolbar = QToolBar(self)
-        toolbar.addWidget(self._create_cursor_button())
-        toolbar.addWidget(self._create_add_annotation_button())
-        return toolbar
 
     def _setup_layout(self) -> None:
         self.scrollArea = QScrollArea(self)
@@ -77,5 +122,5 @@ class EditorWindow(QMainWindow):
         self.label.setPixmap(QPixmap.fromImage(self.image))
         self.scrollArea.setWidget(self.label)
         self.setCentralWidget(self.scrollArea)
-        self.toolbar = self._setup_toolbar()
-        self.addToolBar(self.toolbar)
+        self.controlTollbar = ControllerToolbar(self)
+        self.addToolBar(Qt.ToolBarArea.LeftToolBarArea, self.controlTollbar)
